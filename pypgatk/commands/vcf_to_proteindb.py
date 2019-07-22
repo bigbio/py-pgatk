8ada57fe6902cfcf6844d2055434b23bf2cefb36
husensofteng <husensofteng@gmail.com>
2019-07-22 11:08:49 +0200

    added alias for parameters in vcf_to_proteindb, changed default include_biotypes to all protein coding genes

--- pypgatk/commands/vcf_to_proteindb.py
+++ pypgatk/commands/vcf_to_proteindb.py
@@ -7,17 +7,17 @@ from pypgatk.ensembl.ensembl import EnsemblDataService
 @click.command('vcf-to-proteindb', short_help="Generate peptides based on DNA variants from ENSEMBL VEP VCF files")
 @click.option('--config_file', '-c', help='Configuration to perform conversion between ENSEMBL Files',
               default='config/ensembl_config.yaml')
-@click.option('--input_fasta', help='Path to the transcript sequence')
-@click.option('--vep_annotated_vcf', help='Path to the vep annotated VCF file')
-@click.option('--gene_annotations_gtf', help='Path to the gene annotations file')
-@click.option('--translation_table', default=1, type=int, help="Translation table (Default 1) ")
-@click.option('--mito_translation_table', default=2, type=int, help='Mito_trans_table (default 2)')
-@click.option('--var_prefix', default="var", help="String to add before the variant peptides")
+@click.option('-f', '--input_fasta', help='Path to the transcript sequence')
+@click.option('-v', '--vep_annotated_vcf', help='Path to the vep annotated VCF file')
+@click.option('-g', '--gene_annotations_gtf', help='Path to the gene annotations file')
+@click.option('-t', '--translation_table', default=1, type=int, help="Translation table (Default 1) ")
+@click.option('-m', '--mito_translation_table', default=2, type=int, help='Mito_trans_table (default 2)')
+@click.option('-p', '--var_prefix', default="var", help="String to add before the variant peptides")
 @click.option('--report_ref_seq', help='In addition to var peps, also report all ref peps', is_flag=True)
-@click.option('--output_proteindb', default="peptide-database.fa", help="Output file name, exits if already exists")
+@click.option('-o', '--output_proteindb', default="peptide-database.fa", help="Output file name, exits if already exists")
 @click.option('--annotation_field_name', default="CSQ",
               help="Annotation field name found in the INFO column, e.g CSQ or vep")
-@click.option('--af_field', default="MAF", help="field name in the VCF INFO column to use for filtering on AF")
+@click.option('--af_field', default="", help="field name in the VCF INFO column to use for filtering on AF, (Default None)")
 @click.option('--af_threshold', default=0.01, help='Minium AF threshold for considering common variants')
 @click.option('--transcript_index', default=3, type=int,
               help='Index of transcript ID in the annotated columns (separated by |)')
@@ -27,10 +27,10 @@ from pypgatk.ensembl.ensembl import EnsemblDataService
 @click.option('--exclude_consequences',
               default='downstream_gene_variant, upstream_gene_variant, intergenic_variant, intron_variant, synonymous_variant',
               help="Excluded Consequences", show_default=True)
-@click.option('--skip_including_all_cds',
+@click.option('-s', '--skip_including_all_cds',
               help="by default any transcript that has a defined CDS will be used, this option disables this features instead it only depends on the biotypes",
               is_flag=True)
-@click.option('--include_biotypes', default='', help="Include Biotypes")
+@click.option('--include_biotypes', default='protein_coding,polymorphic_pseudogene,non_stop_decay,nonsense_mediated_decay,IG_C_gene,IG_D_gene,IG_J_gene,IG_V_gene,TR_C_gene,TR_D_gene,TR_J_gene,TR_V_gene,TEC', help="Only genes/transcripts (depending on --biotype_str) from these biotypes are considered for trasnaltion, in addition CDS transcripts unless --skip_including_all_cds is given")
 @click.option('--include_consequences', default='all', help="included_consequences, default all")
 @click.option('--biotype_str', default='transcript_biotype',
               help='string used to identify gene/transcript biotype in the gtf file.')
