81b69b350ff7873050e509f01edfb0b7e7aee3d3
Yasset Perez-Riverol <ypriverol@gmail.com>
2019-08-22 13:32:42 +0100

    added package to dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
         return f.read()
 
 setup(name='pypgatk',
-      version='0.0.4',
+      version='0.0.5',
       description='Python tools for proteogenomics',
       url='http://github.com/bigbio/py-pgatk',
       long_description=readme(),
