2814aae034b86adab57adadb12c54414247e2d37
Yasset Perez-Riverol <ypriverol@gmail.com>
2021-03-06 12:22:54 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 
 setup(name='pypgatk',
-      version='0.0.15',
+      version='0.0.16',
       description='Python tools for proteogenomics',
       url='http://github.com/bigbio/py-pgatk',
       long_description=readme(),
