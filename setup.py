9b15b1a0fe52ed523c72102a5ab04bcea1ef973b
husensofteng <husensofteng@gmail.com>
2021-03-12 15:23:48 +0100

    Updated version to 0.017

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 
 setup(name='pypgatk',
-      version='0.0.16',
+      version='0.0.17',
       description='Python tools for proteogenomics',
       url='http://github.com/bigbio/py-pgatk',
       long_description=readme(),
