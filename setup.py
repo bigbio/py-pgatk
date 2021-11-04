e1c00e63fef6c5fb19b8a80ff20e39f7bc5702df
ypriverol <ypriverol@gmail.com>
2021-11-04 12:35:11 +0000

    sketch of openms analysis

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(name='pypgatk',
         'simplejson==3.16.0',
         'ratelimit==2.2.1',
         'pyteomics==4.4.2',
-        'pybedtools==0.8.2'
+        'pybedtools==0.8.2',
+        'pyopenms'
       ],
       python_requires=">=3.6",
       scripts=['pypgatk/pypgatk_cli.py'],
