e674e6c3306ebaf3d8bed8f15370274e87e20034
ypriverol <ypriverol@gmail.com>
2021-11-04 12:32:49 +0000

    sketch of openms analysis

---
+++ pypgatk/commands/peptide_class_fdr.py
@@ -0,0 +1,32 @@
+import click
+import logging
+from pypgatk.commands.utils import print_help
+
+import pkgutil
+
+from pypgatk.proteomics.openms import OpenmsDataService
+from pypgatk.toolbox.general import read_yaml_from_text, read_yaml_from_file
+default_config_text = pkgutil.get_data(__name__, "../config/openms_analysis.yaml").decode()
+
+
+@click.command('peptide-class-fdr', short_help="Command to compute the Peptide class FDR")
+@click.option('-c', '--config_file', help='Configuration to perform Peptide Class FDR')
+@click.option('-in', '--input_idxml', help='idxml from openms with peptides and proteins')
+@click.option('-out', '--output_idxml', help='idxml from openms with filtered peptides and proteins')
+@click.pass_context
+def peptide_class_fdr(ctx, config_file, input_idxml, output_idxml):
+
+  if config_file is None:
+    config_data = read_yaml_from_text(default_config_text)
+    msg = "The default configuration file is used: {}".format("ensembl_config.yaml")
+    logging.info(msg)
+  else:
+    config_data = read_yaml_from_file(config_file)
+
+  if input_idxml is None or output_idxml is None:
+    print_help()
+
+  pipeline_arguments = {}
+
+  openms_analyzer = OpenmsDataService(config_data, pipeline_arguments)
+  openms_analyzer.filter_peptide_class_fdr(input_idxml, output_idxml)
---
+++ pypgatk/proteomics/openms.py
@@ -0,0 +1,52 @@
+from pyopenms import *
+
+from pypgatk.toolbox.general import ParameterConfiguration
+
+
+class OpenmsDataService(ParameterConfiguration):
+  CONFIG_KEY_OPENMS_ANALYSIS = 'openms_analysis'
+  CONFIG_MIN_PEPTIDE_LENGTH = 'min_peptide_length'
+  CONFIG_DECOY_PREFIX = 'decoy_prefix'
+
+  def __init__(self, config_file, pipeline_arguments):
+    super(OpenmsDataService, self).__init__(self.CONFIG_KEY_OPENMS_ANALYSIS, config_file,
+                                                pipeline_arguments)
+
+    self._decoy_prefix = self.get_default_parameters()[self.CONFIG_KEY_OPENMS_ANALYSIS][
+      self.CONFIG_DECOY_PREFIX]
+    if self.CONFIG_DECOY_PREFIX in self.get_pipeline_parameters():
+      self._decoy_prefix = self.get_pipeline_parameters()[self.CONFIG_DECOY_PREFIX]
+
+
+    self._min_peptide_length = self.get_default_parameters()[self.CONFIG_KEY_OPENMS_ANALYSIS][
+      self.CONFIG_MIN_PEPTIDE_LENGTH]
+    if self.CONFIG_MIN_PEPTIDE_LENGTH in self.get_pipeline_parameters():
+      self._min_peptide_length = self.get_pipeline_parameters()[self.CONFIG_MIN_PEPTIDE_LENGTH]
+
+  def filter_peptide_class_fdr(self, input_idxml, output_idxml):
+    """
+    Filter peptides by Class FDR in idXML files
+    :param input_idxml:  input idxml
+    :param output_idxml: output idxml
+    :return:
+    """
+
+    protein_ids = []
+    peptide_ids = []
+
+    idfilter = IDFilter()
+    IdXMLFile().load(input_idxml, protein_ids, peptide_ids)
+    print(peptide_ids[0])
+
+    # Iterate over PeptideIdentification
+    filtered_peptide_ids = list(filter(lambda peptide: (len(peptide.getHits()[0].getSequence().toUnmodifiedString()) > self._min_peptide_length), peptide_ids))
+    idfilter.removeUnreferencedProteins(protein_ids, filtered_peptide_ids)
+
+    IdXMLFile().store(output_idxml, protein_ids, peptide_ids)
+
+
+
+
+
+
+
