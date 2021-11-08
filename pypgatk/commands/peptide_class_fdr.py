6f092a61cee8b5d6187a219d6a959e342e09d3b0
ypriverol <ypriverol@gmail.com>
2021-11-08 15:58:50 +0000

    first iteration class-FDR for multiple classes

--- pypgatk/commands/peptide_class_fdr.py
+++ pypgatk/commands/peptide_class_fdr.py
@@ -1,3 +1,5 @@
+import json
+
 import click
 import logging
 from pypgatk.commands.utils import print_help
@@ -8,6 +10,17 @@ from pypgatk.proteomics.openms import OpenmsDataService
 from pypgatk.toolbox.general import read_yaml_from_text, read_yaml_from_file
 default_config_text = pkgutil.get_data(__name__, "../config/openms_analysis.yaml").decode()
 
+def parse_peptide_groups(peptide_groups_prefix):
+  peptide_groups = {}
+  for group in peptide_groups_prefix.split(";"):
+    lt = group.split(":")
+    class_group = lt[0].replace("{","")
+    classes = [x.replace("[","").replace("]","") for x in lt[1].split(",")]
+    peptide_groups[class_group] = classes
+  return peptide_groups
+
+
+
 
 @click.command('peptide-class-fdr', short_help="Command to compute the Peptide class FDR")
 @click.option('-c', '--config_file', help='Configuration to perform Peptide Class FDR')
@@ -16,8 +29,10 @@ default_config_text = pkgutil.get_data(__name__, "../config/openms_analysis.yaml
 @click.option('--min-peptide-length', help='minimum peptide length')
 @click.option('--psm-pep-fdr-cutoff', help="PSM peptide FDR cutoff or threshold", default=0.01)
 @click.option('--psm-pep-class-fdr-cutoff', help="PSM class peptide FDR cutoff or threshold", default=0.01)
+@click.option('--peptide_groups_prefix', help="Peptide class "
+              "groups e.g. \"{non_canonical:[altorf,pseudo,ncRNA];mutations:[COSMIC,cbiomut];variants:[var_mut,var_rs]}\"")
 @click.pass_context
-def peptide_class_fdr(ctx, config_file, input_idxml, output_idxml, min_peptide_length, psm_pep_fdr_cutoff, psm_pep_class_fdr_cutoff):
+def peptide_class_fdr(ctx, config_file, input_idxml, output_idxml, min_peptide_length, psm_pep_fdr_cutoff, psm_pep_class_fdr_cutoff, peptide_groups_prefix):
 
   if config_file is None:
     config_data = read_yaml_from_text(default_config_text)
@@ -30,6 +45,20 @@ def peptide_class_fdr(ctx, config_file, input_idxml, output_idxml, min_peptide_l
     print_help()
 
   pipeline_arguments = {}
+
+  if min_peptide_length is not None:
+    pipeline_arguments[OpenmsDataService.CONFIG_MIN_PEPTIDE_LENGTH] = min_peptide_length
+
+  if psm_pep_fdr_cutoff is not None:
+    pipeline_arguments[OpenmsDataService.CONFIG_PEPTIDE_FDR_CUTOFF] = psm_pep_fdr_cutoff
+
+
+  if psm_pep_class_fdr_cutoff is not None:
+    pipeline_arguments[OpenmsDataService.CONFIG_PEPTIDE_CLASS_FDR_CUTOFF] = psm_pep_class_fdr_cutoff
+
+  if peptide_groups_prefix is not None:
+    data = parse_peptide_groups(peptide_groups_prefix)
+    pipeline_arguments[OpenmsDataService.CONFIG_PEPTIDE_GROUP_PREFIX] = data
 
 
   openms_analyzer = OpenmsDataService(config_data, pipeline_arguments)
--- pypgatk/proteomics/openms.py
+++ pypgatk/proteomics/openms.py
@@ -1,6 +1,7 @@
 from pyopenms import *
 
 from pypgatk.toolbox.general import ParameterConfiguration
+import numpy.polynomial.polynomial as poly
 
 
 class OpenmsDataService(ParameterConfiguration):
@@ -10,6 +11,7 @@ class OpenmsDataService(ParameterConfiguration):
   CONFIG_PEPTIDE_CLASS_PREFIX = "peptide_classes_prefix"
   CONFIG_PEPTIDE_FDR_CUTOFF = "psm_pep_fdr_cutoff"
   CONFIG_PEPTIDE_CLASS_FDR_CUTOFF = "psm_pep_class_fdr_cutoff"
+  CONFIG_PEPTIDE_GROUP_PREFIX = "peptide_groups_prefix"
 
   def __init__(self, config_file, pipeline_arguments):
     super(OpenmsDataService, self).__init__(self.CONFIG_KEY_OPENMS_ANALYSIS, config_file,
@@ -40,6 +42,19 @@ class OpenmsDataService(ParameterConfiguration):
     if self.CONFIG_PEPTIDE_CLASS_FDR_CUTOFF in self.get_pipeline_parameters():
       self._psm_pep_fdr_cutoff = self.get_pipeline_parameters()[self.CONFIG_PEPTIDE_CLASS_FDR_CUTOFF]
 
+    self._peptide_groups_prefix = self.get_default_parameters()[self.CONFIG_KEY_OPENMS_ANALYSIS][
+      self.CONFIG_PEPTIDE_GROUP_PREFIX]
+    if self.CONFIG_PEPTIDE_GROUP_PREFIX in self.get_pipeline_parameters():
+      self._peptide_groups_prefix = self.get_pipeline_parameters()[self.CONFIG_PEPTIDE_GROUP_PREFIX]
+
+  def is_peptide_group(self, peptide_group_members, accessions):
+    accession_group = 0
+    for accession in accessions:
+      for class_peptide in peptide_group_members:
+        if class_peptide in accession:
+          accession_group += 1
+    return len(accessions) == accession_group
+
   def compute_global_fdr(self, peptide_ids):
     """
     Compute the global FDR and filter peptides
@@ -65,7 +80,7 @@ class OpenmsDataService(ParameterConfiguration):
     peptide_hit_dict.sort(key=lambda x: x[0].getScore())
 
     for peptide_tuple in peptide_hit_dict:
-      (psm, peptide_identificaion) = peptide_tuple
+      (psm, peptide_identification) = peptide_tuple
       accessions = [ev.getProteinAccession() for ev in psm.getPeptideEvidences()]
 
       if any(self._decoy_prefix in s for s in accessions):
@@ -75,10 +90,10 @@ class OpenmsDataService(ParameterConfiguration):
 
       FDR = float(decoy_count) / float(target_count)
       if FDR < self._psm_pep_fdr_cutoff:
-        identifier = peptide_identificaion.getMetaValue("spectrum_reference")
+        identifier = peptide_identification.getMetaValue("spectrum_reference")
         print(identifier)
         if identifier not in peptides_filtered:
-          peptide_id = peptide_identificaion
+          peptide_id = peptide_identification
           peptide_id.setHits([psm])
           peptides_filtered[identifier] = peptide_id
         else:
@@ -93,12 +108,21 @@ class OpenmsDataService(ParameterConfiguration):
     return list(peptides_filtered.values())
 
   def compute_class_fdr(self, peptide_ids):
+    """
+    Compute the global FDR and filter peptides
+    :param peptide_ids: list of peptide identifications
+    :return:  filtered peptides
+    """
+
+    target_count = 0
+    decoy_count = 0
+
+    # Map list of peptides with hits to -> map(hit, peptide)
+    peptide_hit_dict = []
 
     # Here the Map contains Key=spectrum_reference (unique for each PeptideIdentification)
     # Value is the PeptideIdentification
-
     peptides_filtered = {}
-    peptide_hit_dict = []
 
     for peptide in peptide_ids:
       for peptide_hit in peptide.getHits():
@@ -107,8 +131,102 @@ class OpenmsDataService(ParameterConfiguration):
     # Todo: Here would be interesting to know the score order isHigherScoreBetter
     peptide_hit_dict.sort(key=lambda x: x[0].getScore())
 
+    score_dic = {}
+    decoy_dic = {}
+
+    # A dictionary of peptide classes with number of decoys and targets per class
+    novel_target_decoy_count = {}
+    for peptide_class in self._peptide_groups_prefix:
+      novel_target_decoy_count[peptide_class] = (0,0)
+      decoy_dic[peptide_class] = {}
+      score_dic[peptide_class] = {}
+
+    global_decoy_count = 0
+    global_target_count = 0
+
+    novpep_dic = {}
+
     for peptide_hit in peptide_hit_dict:
-      accessions = [ev.getProteinAccession() for ev in peptide_hit[0].getPeptideEvidences()]
+      (psm, peptide_identification) = peptide_hit
+      accessions = [ev.getProteinAccession() for ev in psm.getPeptideEvidences()]
+      score = -np.log10(float(psm.getScore()))
+      if any(self._decoy_prefix in s for s in accessions):
+        global_decoy_count += 1
+        for peptide_group in self._peptide_groups_prefix:
+          if self.is_peptide_group(self._peptide_groups_prefix[peptide_group], accessions):
+            (target, decoy) = novel_target_decoy_count[peptide_group]
+            decoy += 1
+            novel_target_decoy_count[peptide_group] = (target, decoy)
+            decoy_dic[peptide_group][score] = [decoy, global_decoy_count]
+      else:
+        global_target_count += 1
+        for peptide_group in self._peptide_groups_prefix:
+          if self.is_peptide_group(self._peptide_groups_prefix[peptide_group], accessions):
+            (target, decoy) = novel_target_decoy_count[peptide_group]
+            target += 1
+            novel_target_decoy_count[peptide_group] = (target, decoy)
+
+      for peptide_group in self._peptide_groups_prefix:
+        (target, decoy) = novel_target_decoy_count[peptide_group]
+        score_dic[peptide_group][score] = [global_target_count, global_decoy_count, target, decoy]
+
+    print(score_dic)
+
+
+    peptide_dict_models = {}
+    count = 0
+    for peptide_class in score_dic:
+      x = []
+      y = []
+      x_filter = []
+      y_filter = []
+      for score in decoy_dic[peptide_class]:
+        x.append(score)
+        frac = float(decoy_dic[peptide_class][score][0] / decoy_dic[peptide_class][score][1])
+        y.append(frac)
+        # Todo: In the original algorithm https://github.com/yafeng/proteogenomics_python/blob/4b1638aa75903225e9ae45892af4cb9f078d7421/BayesClassSpecificFDR.py#L88 the authors used a filter for the score between 6-10
+        # if 6 < score < 10:
+        #   x_filter.append(score)
+        #   y_filter.append(frac)
+        x_filter.append(score)
+        y_filter.append(frac)
+      if len(x_filter) > 0 and len(y_filter) > 0:
+        coefs = poly.polyfit(np.array(x_filter), np.array(y_filter), 1)
+        print("coefficients", coefs)
+        fit = poly.polyval(x, coefs)
+        peptide_dict_models[peptide_class] = (coefs, fit)
+
+    peptides_filtered = {}
+    for peptide_hit in peptide_hit_dict:
+      (psm, peptide_identification) = peptide_hit
+      accessions = [ev.getProteinAccession() for ev in psm.getPeptideEvidences()]
+      score = -np.log10(float(psm.getScore()))
+      for peptide_class in peptide_dict_models:
+        if self.is_peptide_group(self._peptide_groups_prefix[peptide_class], accessions):
+          counts = score_dic[peptide_class][score]
+          global_target_count = float(counts[0])
+          global_decoy_count = float(counts[1])
+          FDR = global_decoy_count / global_target_count
+          novel_targetcount = float(counts[2])
+          gamma = poly.polyval(score, peptide_dict_models[peptide_class][0])
+          novelFDR = FDR * gamma * (global_target_count / novel_targetcount)
+          if novelFDR < self._psm_pep_class_fdr_cutoff and FDR < self._psm_pep_fdr_cutoff:
+            identifier = peptide_identification.getMetaValue("spectrum_reference")
+            if identifier not in peptides_filtered:
+              peptide_id = peptide_identification
+              peptide_id.setHits([psm])
+              peptides_filtered[identifier] = peptide_id
+            else:
+              peptide_id = peptides_filtered[identifier]
+              hits = peptide_id.getHits()
+              hits.append(psm)
+              peptide_id.setHits(hits)
+              peptides_filtered[identifier] = peptide_id
+            count += 1
+            print("Peptide Class {}, Peptide Sequence: {}, Protein Accessions {}, NovelFDR {}, count {}".format(peptide_class, psm.getSequence().toUnmodifiedString(), accessions, novelFDR, count))
+
+
+    return list(peptides_filtered.values())
 
   def filter_peptide_class_fdr(self, input_idxml, output_idxml):
     """
@@ -130,11 +248,15 @@ class OpenmsDataService(ParameterConfiguration):
       filter(lambda peptide: (len(peptide.getHits()[0].getSequence().toUnmodifiedString()) > self._min_peptide_length),
              peptide_ids))
 
-    filtered_peptide_ids = self.compute_global_fdr(filtered_peptide_ids)
+    # filtered_peptide_ids = self.compute_global_fdr(filtered_peptide_ids)
+    filtered_peptide_ids = self.compute_class_fdr(filtered_peptide_ids)
 
     remove_peptides_without_reference = True
     idfilter.updateProteinReferences(filtered_peptide_ids, protein_ids, remove_peptides_without_reference)
     IdXMLFile().store(output_idxml, protein_ids, filtered_peptide_ids)
+
+
+
 
 
 
