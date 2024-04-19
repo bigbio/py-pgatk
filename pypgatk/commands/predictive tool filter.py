805c592ca47d061658d96345ecaf6950da361839
Yasset Perez-Riverol <ypriverol@gmail.com>
2024-04-19 21:08:03 +0100

    skip to tests not working with pytests pooling.

--- pypgatk/commands/predictive tool filter.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-import click
-
-from pypgatk.toolbox.general import read_yaml_from_file
-from pypgatk.commands.utils import print_help
-from pypgatk.proteogenomics.blast_get_position import BlastGetPositionService
